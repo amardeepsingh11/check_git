9fa0dab25e5482abbf1d4ec3faa8a01eaaf8d3ff
amardeepsingh11 <amar.sahdev@gmail.com>
2017-11-25 18:40:20 +1100

    updated knife.rb

--- chef/knife.rb
+++ chef/knife.rb
@@ -3,7 +3,7 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "devops11"
+node_name                "devops12"
 client_key               "#{current_dir}/devops11.pem"
 chef_server_url          "https://api.chef.io/organizations/devops11"
 cookbook_path            ["#{current_dir}/../cookbooks"]
